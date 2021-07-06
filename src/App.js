d0bbf754012d4f1fe5fb5aa446e53fecc5f1e7df
Connor Rayburn <connorrayburn.work@gmail.com>
2021-07-06 15:03:31 -0700

    set up user card

--- src/App.js
+++ src/App.js
@@ -5,24 +5,44 @@ import axios from 'axios';
 class App extends React.Component {
 
   state = {
-    user: []
+    user: {
+      name:'',
+      bio:'',
+      avatar:'',
+      url:''
+    },
+    follower: {
+      user:'',
+      avatar:'',
+      url:''
+    }
   }
 
   componentDidMount() {
     axios.get('https://api.github.com/users/crayburn18')
       .then(res => {
         console.log(res.data)
-      })
+      }).catch(err => {
+        console.log(err)
+      });
 
     axios.get('https://api.github.com/users/crayburn18/followers')
       .then(res => {
         console.log(res.data)
-      })
+      }).catch(err => {
+        console.log(err)
+      });
   }
 
   render() {
     return (
-      <div>Hello</div>
+      <div>
+        <div>
+          <h1>User Card</h1>
+          <img src="https://avatars.githubusercontent.com/u/61172495?v=4"></img>
+          <div></div>
+        </div>
+      </div>
     );
   }
 }
