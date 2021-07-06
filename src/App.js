f641b422150bb7766b664ae691800043f088e4a9
Connor Rayburn <connorrayburn.work@gmail.com>
2021-07-06 13:36:22 -0700

    axios calls for user and followers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
+class App extends React.Component {
 
-    }
-  };
+  state = {
+    user: []
+  }
 
-  render(){
-    return(
+  componentDidMount() {
+    axios.get('https://api.github.com/users/crayburn18')
+      .then(res => {
+        console.log(res.data)
+      })
+
+    axios.get('https://api.github.com/users/crayburn18/followers')
+      .then(res => {
+        console.log(res.data)
+      })
+  }
+
+  render() {
+    return (
       <div>Hello</div>
     );
   }
-
 }
+
 
 export default App;
